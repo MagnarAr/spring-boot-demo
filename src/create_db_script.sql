d0a8df57357af6fb49912c4585ebdec5fdf6f76b
Magnar Aruoja <magnar.aruoja@gmail.com>
2018-03-07 17:19:30 +0200

    Add db creation script

---
+++ src/create_db_script.sql
@@ -0,0 +1,20 @@
+create table subscription
+(
+	subscription_id serial not null
+		constraint subscription_pkey
+			primary key,
+	type varchar(10) not null,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	email varchar(100) not null
+)
+;
+
+create unique index subscription_subscription_id_uindex
+	on subscription (subscription_id)
+;
+
+create unique index subscription_email_uindex
+	on subscription (email)
+;
+
