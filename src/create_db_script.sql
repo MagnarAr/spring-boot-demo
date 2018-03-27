78b0138b6de90f76d57e8d0f7a46ea3431b9d2c8
Magnar Aruoja <magnar.aruoja@gmail.com>
2018-03-28 00:20:47 +0300

    update db create script

--- src/create_db_script.sql
+++ src/create_db_script.sql
@@ -6,7 +6,8 @@ create table subscription
 	type varchar(10) not null,
 	first_name varchar(50) not null,
 	last_name varchar(50) not null,
-	email varchar(100) not null
+	email varchar(100) not null,
+	image_source varchar
 )
 ;
 
