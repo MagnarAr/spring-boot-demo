6c8a6ff41236e1de3af6d4a1542158515fd055b5
Magnar Aruoja <magnar.aruoja@gmail.com>
2018-05-02 11:06:21 +0300

    Google Auth Example

--- src/main/java/ee/ut/cs/wad/springbootdemo/application/config/SecurityConfiguration.java
+++ src/main/java/ee/ut/cs/wad/springbootdemo/application/config/SecurityConfiguration.java
@@ -1,14 +1,42 @@
 package ee.ut.cs.wad.springbootdemo.application.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableOAuth2Sso
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private OAuth2ClientContext oauth2ClientContext;
+    private AuthorizationCodeResourceDetails authorizationCodeResourceDetails;
+    private ResourceServerProperties resourceServerProperties;
+
+    @Autowired
+    public void setOauth2ClientContext(OAuth2ClientContext oauth2ClientContext) {
+        this.oauth2ClientContext = oauth2ClientContext;
+    }
+
+    @Autowired
+    public void setAuthorizationCodeResourceDetails(AuthorizationCodeResourceDetails authorizationCodeResourceDetails) {
+        this.authorizationCodeResourceDetails = authorizationCodeResourceDetails;
+    }
+
+    @Autowired
+    public void setResourceServerProperties(ResourceServerProperties resourceServerProperties) {
+        this.resourceServerProperties = resourceServerProperties;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,9 +46,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // with spring security, we authorize all requests except the paths described in "resources" array above
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/greeting").authenticated()
+                .antMatchers("/**").permitAll()
+                // google auth filter
+                .and().addFilterAt(filter(), BasicAuthenticationFilter.class)
+                .logout().logoutSuccessUrl("/subscription");
 //                .antMatchers(HttpMethod.GET, resources).permitAll()
 //                .antMatchers(HttpMethod.POST, resources).permitAll()
 //                .anyRequest().authenticated().and().formLogin().permitAll();
+    }
+
+    /*This method for creating filter for OAuth authentication.*/
+    private OAuth2ClientAuthenticationProcessingFilter filter() {
+        //Creating the filter for "/google/login" url
+        OAuth2ClientAuthenticationProcessingFilter oAuth2Filter = new OAuth2ClientAuthenticationProcessingFilter(
+                "/google/login");
+
+        //Creating the rest template for getting connected with OAuth service.
+        //The configuration parameters will inject while creating the bean.
+        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(authorizationCodeResourceDetails,
+                oauth2ClientContext);
+        oAuth2Filter.setRestTemplate(oAuth2RestTemplate);
+        // Setting the token service. It will help for getting the token and
+        // user details from the OAuth Service.
+        oAuth2Filter.setTokenServices(new UserInfoTokenServices(resourceServerProperties.getUserInfoUri(),
+                resourceServerProperties.getClientId()));
+
+        return oAuth2Filter;
     }
 }
